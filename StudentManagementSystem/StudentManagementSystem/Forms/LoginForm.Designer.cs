d42d95b3a47ad99cb59f28b0b0c2676586f33755
k-kukes <158488017+k-kukes@users.noreply.github.com>
2025-05-10 13:55:28 -0400

    Created DataSet & Login code logic

--- StudentManagementSystem/StudentManagementSystem/Forms/LoginForm.Designer.cs
+++ StudentManagementSystem/StudentManagementSystem/Forms/LoginForm.Designer.cs
@@ -31,14 +31,15 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginPageForm));
             this.titleLabel = new System.Windows.Forms.Label();
             this.loginBox = new System.Windows.Forms.GroupBox();
-            this.loginLabel = new System.Windows.Forms.Label();
-            this.usernameTextBox = new System.Windows.Forms.TextBox();
-            this.passwordTextBox = new System.Windows.Forms.TextBox();
-            this.noAccountInfoLabel = new System.Windows.Forms.Label();
-            this.createAccountLinkLabel = new System.Windows.Forms.LinkLabel();
             this.logInButton = new System.Windows.Forms.Button();
+            this.createAccountLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.noAccountInfoLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.loginLabel = new System.Windows.Forms.Label();
             this.frenchButton = new System.Windows.Forms.Button();
             this.lightModeButton = new System.Windows.Forms.Button();
+            this.incorrectInfoLabel = new System.Windows.Forms.Label();
             this.loginBox.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,6 +56,7 @@
             // loginBox
             // 
             this.loginBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.loginBox.Controls.Add(this.incorrectInfoLabel);
             this.loginBox.Controls.Add(this.logInButton);
             this.loginBox.Controls.Add(this.createAccountLinkLabel);
             this.loginBox.Controls.Add(this.noAccountInfoLabel);
@@ -63,70 +65,73 @@
             this.loginBox.Controls.Add(this.loginLabel);
             this.loginBox.Location = new System.Drawing.Point(28, 75);
             this.loginBox.Name = "loginBox";
-            this.loginBox.Size = new System.Drawing.Size(403, 187);
+            this.loginBox.Size = new System.Drawing.Size(403, 206);
             this.loginBox.TabIndex = 1;
             this.loginBox.TabStop = false;
             // 
-            // loginLabel
+            // logInButton
             // 
-            this.loginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginLabel.Location = new System.Drawing.Point(148, 17);
-            this.loginLabel.Name = "loginLabel";
-            this.loginLabel.Size = new System.Drawing.Size(100, 23);
-            this.loginLabel.TabIndex = 2;
-            this.loginLabel.Text = "Login";
-            this.loginLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // usernameTextBox
-            // 
-            this.usernameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.usernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.usernameTextBox.Location = new System.Drawing.Point(91, 53);
-            this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(220, 22);
-            this.usernameTextBox.TabIndex = 3;
-            this.usernameTextBox.Text = "Enter your username:";
-            this.usernameTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // passwordTextBox
-            // 
-            this.passwordTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordTextBox.Location = new System.Drawing.Point(91, 91);
-            this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.Size = new System.Drawing.Size(220, 22);
-            this.passwordTextBox.TabIndex = 4;
-            this.passwordTextBox.Text = "Enter your password:";
-            this.passwordTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // noAccountInfoLabel
-            // 
-            this.noAccountInfoLabel.AutoSize = true;
-            this.noAccountInfoLabel.Location = new System.Drawing.Point(88, 125);
-            this.noAccountInfoLabel.Name = "noAccountInfoLabel";
-            this.noAccountInfoLabel.Size = new System.Drawing.Size(122, 13);
-            this.noAccountInfoLabel.TabIndex = 5;
-            this.noAccountInfoLabel.Text = "Don\'t have an account?";
+            this.logInButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logInButton.Location = new System.Drawing.Point(236, 164);
+            this.logInButton.Name = "logInButton";
+            this.logInButton.Size = new System.Drawing.Size(75, 36);
+            this.logInButton.TabIndex = 7;
+            this.logInButton.Text = "Log In";
+            this.logInButton.UseVisualStyleBackColor = true;
+            this.logInButton.Click += new System.EventHandler(this.logInButton_Click);
             // 
             // createAccountLinkLabel
             // 
             this.createAccountLinkLabel.AutoSize = true;
-            this.createAccountLinkLabel.Location = new System.Drawing.Point(115, 148);
+            this.createAccountLinkLabel.Location = new System.Drawing.Point(115, 187);
             this.createAccountLinkLabel.Name = "createAccountLinkLabel";
             this.createAccountLinkLabel.Size = new System.Drawing.Size(95, 13);
             this.createAccountLinkLabel.TabIndex = 6;
             this.createAccountLinkLabel.TabStop = true;
             this.createAccountLinkLabel.Text = "Create an account";
             // 
-            // logInButton
+            // noAccountInfoLabel
             // 
-            this.logInButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.logInButton.Location = new System.Drawing.Point(236, 125);
-            this.logInButton.Name = "logInButton";
-            this.logInButton.Size = new System.Drawing.Size(75, 36);
-            this.logInButton.TabIndex = 7;
-            this.logInButton.Text = "Log In";
-            this.logInButton.UseVisualStyleBackColor = true;
+            this.noAccountInfoLabel.AutoSize = true;
+            this.noAccountInfoLabel.Location = new System.Drawing.Point(88, 164);
+            this.noAccountInfoLabel.Name = "noAccountInfoLabel";
+            this.noAccountInfoLabel.Size = new System.Drawing.Size(122, 13);
+            this.noAccountInfoLabel.TabIndex = 5;
+            this.noAccountInfoLabel.Text = "Don\'t have an account?";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextBox.Location = new System.Drawing.Point(91, 128);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(220, 22);
+            this.passwordTextBox.TabIndex = 4;
+            this.passwordTextBox.Text = "Enter your password";
+            this.passwordTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.passwordTextBox.TextChanged += new System.EventHandler(this.passwordTextBox_TextChanged);
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.usernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTextBox.Location = new System.Drawing.Point(91, 90);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(220, 22);
+            this.usernameTextBox.TabIndex = 3;
+            this.usernameTextBox.Text = "Enter your username:";
+            this.usernameTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.usernameTextBox.TextChanged += new System.EventHandler(this.usernameTextBox_TextChanged);
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginLabel.Location = new System.Drawing.Point(149, 16);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(100, 23);
+            this.loginLabel.TabIndex = 2;
+            this.loginLabel.Text = "Login";
+            this.loginLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // frenchButton
             // 
@@ -149,6 +154,19 @@
             this.lightModeButton.Size = new System.Drawing.Size(31, 30);
             this.lightModeButton.TabIndex = 3;
             this.lightModeButton.UseVisualStyleBackColor = true;
+            // 
+            // incorrectInfoLabel
+            // 
+            this.incorrectInfoLabel.BackColor = System.Drawing.Color.White;
+            this.incorrectInfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.incorrectInfoLabel.ForeColor = System.Drawing.Color.Red;
+            this.incorrectInfoLabel.Location = new System.Drawing.Point(88, 39);
+            this.incorrectInfoLabel.Name = "incorrectInfoLabel";
+            this.incorrectInfoLabel.Size = new System.Drawing.Size(223, 39);
+            this.incorrectInfoLabel.TabIndex = 8;
+            this.incorrectInfoLabel.Text = "Incorrect username or password! Try Again!";
+            this.incorrectInfoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.incorrectInfoLabel.Visible = false;
             // 
             // loginPageForm
             // 
@@ -180,6 +198,7 @@
         private System.Windows.Forms.Label noAccountInfoLabel;
         private System.Windows.Forms.Button frenchButton;
         private System.Windows.Forms.Button lightModeButton;
+        private System.Windows.Forms.Label incorrectInfoLabel;
     }
 }
 
--- StudentManagementSystem/StudentManagementSystem/Forms/LoginForm.cs
+++ StudentManagementSystem/StudentManagementSystem/Forms/LoginForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using StudentManagementSystem.StudentDBDataSetTableAdapters;
 
 namespace StudentManagementSystem
 {
@@ -15,6 +16,37 @@ namespace StudentManagementSystem
         public loginPageForm()
         {
             InitializeComponent();
+        }
+
+        private void usernameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            incorrectInfoLabel.Visible = false;
+        }
+
+        private void passwordTextBox_TextChanged(object sender, EventArgs e)
+        {
+            incorrectInfoLabel.Visible = false;
+        }
+
+        private void logInButton_Click(object sender, EventArgs e)
+        {
+            StudentDBDataSet db = new StudentDBDataSet();
+            var adapter = new StudentDBDataSetTableAdapters.UsersTableAdapter();
+            adapter.Fill(db.Users);
+
+            int count = db.Users.Count(row => row.Username == usernameTextBox.Text && row.Password == passwordTextBox.Text);
+
+            if (count > 0)
+            {
+                MessageBox.Show("Succesfull login");
+            }
+            else if (true)
+            {
+                MessageBoxButtons button = MessageBoxButtons.OK;
+                MessageBoxIcon icon = MessageBoxIcon.Error;
+                incorrectInfoLabel.Visible = true;
+                MessageBox.Show("Incorrect username or password!", "Error", button, icon);
+            }
         }
     }
 }
